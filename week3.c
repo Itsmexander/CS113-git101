6af0417d326435b61e520919484d8b58a48a73f7
Itsmexander <chawish.p@ku.th>
2020-02-08 22:12:20 +0700

    add week3.c

---
+++ week3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    char name[50],age_str[3],sex[2];
+    int age;
+    printf("Enter your name: ");
+    gets(name);
+    printf("Hello There ");
+    puts(name);
+}
