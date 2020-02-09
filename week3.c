195eff300e43c734a8c81acc4e4ad07289d4f75a
Itsmexander <chawish.p@ku.th>
2020-02-09 17:52:49 +0700

    update week3.c with while loop

--- week3.c
+++ week3.c
@@ -7,4 +7,22 @@ int main(){
     gets(name);
     printf("Hello There ");
     puts(name);
+    
+    int n_digits = 0;
+    int n_non_digits = 0;
+    int ch;
+    while ((ch=getchar()) != EOF) {
+       switch (ch) {
+          case '0': case '1': case '2': case '3': 
+          case '4': case '5': case '6': case '7':
+          case '8': case '9':
+             n_digits++;
+             break;
+          default: 
+             n_non_digits++;
+             break;
+       }   
+    }
+    printf("จำนวนตัวเลข: %d, ไม่ใช่ตัวเลข: %d\n", n_digits, n_non_digits);
+
 }
