4168764f814ed0c06864ec947662cd1737722359
Itsmexander <chawish.p@ku.th>
2020-02-09 21:08:06 +0700

    Add week4.c with Recursion

--- week4.c
+++ week4.c
@@ -18,10 +18,10 @@
 
 long fib (int  n ) {
     if (n < 1){
-        return     0 ;
+        return 0 ;
     }
     else if(n < 3){
-        return     1;
+        return 1;
     }
 return     fib (n-1) + fib (n-2);
 }
